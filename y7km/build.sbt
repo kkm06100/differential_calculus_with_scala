5f522f54fc62e162698bfb613b27bb0ac2f06dde
김경민 <kkm07133@naver.com>
2025-05-06 22:32:56 +0900

    방정식 출력 IO로 구현

--- y7km/build.sbt
+++ y7km/build.sbt
@@ -4,5 +4,6 @@ ThisBuild / scalaVersion := "2.13.16"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "y7km"
+      name := "y7km",
+      libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.4"
   )
--- y7km/src/main/scala/Main.scala
+++ y7km/src/main/scala/Main.scala
@@ -1,27 +1,50 @@
 import ast._
+import cats.effect.{IO, IOApp}
 
-object Main {
+object Main extends IOApp.Simple {
 
-  def main(args: Array[String]): Unit = {
+  def defineFunction: IO[Expr[Double]] = IO {
     val x = Id()
     val const = Const[Double](5.0)
     val square = Mul(x, x)
-
-    val f = Add(square, const)
-    val df = d(f) // 'f
-
-    val value = df.eval((3.0, 1.0)) // (x, dx) = (3, 1) == (x값, 변화량)
-    println(s"f'(3) = $value")      // expected: 6.0
+    Add(square, const) // f(x) = x^3 + 5
   }
 
+  def derive(f: Expr[Double]): IO[Expr[(Double, Double)]] = IO {
+    d(f)
+  }
+
+  def printResults(f: Expr[Double], df: Expr[(Double, Double)]): IO[Unit] = for {
+    _ <- IO.println(s"f(x)   = ${prettyPrint(f)}")
+    _ <- IO.println(s"f'(x)  = ${prettyPrint(df)}")
+    result = df.eval((3.0, 1.0)) // (x, dx)
+    _ <- IO.println(s"f'(3)  = $result")
+  } yield ()
+
+  override def run: IO[Unit] = for {
+    f  <- defineFunction
+    df <- derive(f)
+    _  <- printResults(f, df)
+  } yield ()
+
   def d[A](f: Expr[A]): Expr[(A, A)] = f match {
-    case Const(_)      => Const[(A, A)](0.0)
-    case Id()          => Proj2[A]()
-    case Add(f1, f2)   => Add(d(f1), d(f2))
-    case Mul(f1, f2)   =>
+    case Const(_)    => Const[(A, A)](0.0)
+    case Id()        => Proj2[A]()
+    case Add(f1, f2) => Add(d(f1), d(f2))
+    case Mul(f1, f2) =>
       Add(
-        Mul(d(f1), Compose(f2, Proj1[A]())), // df1(x, dx) * g(x)
-        Mul(Compose(f1, Proj1[A]()), d(f2))  // f(x) * dg(x, dx)
+        Mul(d(f1), Compose(f2, Proj1[A]())),
+        Mul(Compose(f1, Proj1[A]()), d(f2))
       )
+  }
+
+  def prettyPrint[A](expr: Expr[A]): String = expr match {
+    case Const(v)      => v.toString
+    case Id()          => "x"
+    case Proj1()       => "x"
+    case Proj2()       => "dx"
+    case Add(l, r)     => s"(${prettyPrint(l)} + ${prettyPrint(r)})"
+    case Mul(l, r)     => s"(${prettyPrint(l)} * ${prettyPrint(r)})"
+    case Compose(f, g) => s"${prettyPrint(f)} ∘ ${prettyPrint(g)}"
   }
 }
